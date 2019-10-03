04219db296e14bfcffde0bda83e804c11f081a79
pivanics <ivanics.peter@webstar.hu>
2019-10-03 09:10:36 +0200

    Added first attempt for SwiftUI

--- {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/AppDelegate.swift
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/AppDelegate.swift
@@ -7,18 +7,14 @@
 
 import UIKit
 
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    var window: UIWindow?
     lazy private var router = RootRouter()
     lazy private var deeplinkHandler = DeeplinkHandler()
     lazy private var notificationsHandler = NotificationsHandler()
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.makeKeyAndVisible()
-
         // Notifications
         notificationsHandler.configure()
 
@@ -44,5 +40,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // To enable full remote notifications functionality you should first register the device with your api service
         //https://developer.apple.com/library/content/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/
         notificationsHandler.handleRemoteNotification(with: userInfo)
+    }
+
+    // MARK: UISceneSession Lifecycle
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 }
---
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/Modules/Main/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  {{cookiecutter.app_name}}
+//
+//  Copyright © {{cookiecutter.company_name}}. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
